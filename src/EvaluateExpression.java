97aa888599513217ffc35a956ac1a5e76ada9b22
bsauce <benjaminsaucedo7@gmail.com>
2024-07-12 23:47:25 -0700

    finished hw2

--- src/EvaluateExpression.java
+++ src/EvaluateExpression.java
@@ -1,7 +1,6 @@
 package chapter20;
 
 import java.util.Stack;
-
 import javafx.application .Application;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
@@ -23,7 +22,6 @@ public class EvaluateExpression extends Application {
     }
     BorderPane bp = new BorderPane();
     TextField tf = new TextField();
-
     TextArea text = new TextArea();
 
     public void computeExpression(ActionEvent e) {
@@ -33,47 +31,40 @@ public class EvaluateExpression extends Application {
         // test case (1+2)*4-3
 
         text.clear();
-
         Integer result = evaluateExpression(tf.getText());
-
         text.appendText(result.toString());
 
     }
 
     /** Evaluate an expression */
-
     public int evaluateExpression(String expression) {
 
         // Create operandStack to store operands
-
         Stack<Integer> operandStack = new Stack<>();
 
         // Create operatorStack to store operators
-
         Stack<Character> operatorStack = new Stack<>();
 
         // Insert blanks around (, ), +, -, /, and *
-
         expression = insertBlanks(expression);
 
         // Extract operands and operators
-
         String[] tokens = expression.split("\\s+");
 
         // Phase 1 Scan tokens
-
         for (String token : tokens) {
             if (token.length() == 0) // Blank space
                 continue; // Back to the while loop to extract the next token
 
-                // Phase 1.2
-
+            // Phase 1.2
             else if (token.charAt(0) == '+' || token.charAt(0) == '-') {
                 while (!operatorStack.isEmpty()
                         && (operatorStack.peek() == '+'
                         || operatorStack.peek() == '-'
                         || operatorStack.peek() == '*'
-                        || operatorStack.peek() == '/')) {
+                        || operatorStack.peek() == '/'
+                        || operatorStack.peek() == '%'
+                        || operatorStack.peek() == '^')) {
 
                     processAnOperator(operandStack, operatorStack);
                 }
@@ -81,38 +72,42 @@ public class EvaluateExpression extends Application {
                 // Push the + or - operator into the operator stack
                 operatorStack.push(token.charAt(0));
 
-                // Phase 1.3
-            } else if (token.charAt(0) == '*' || token.charAt(0) == '/') {
+            // Phase 1.3
+            } else if (token.charAt(0) == '*' || token.charAt(0) == '/' || token.charAt(0) == '%') {
 
                 // Process all *, /, % in the top of the operator stack
-
                 while (!operatorStack.isEmpty()
                         && (operatorStack.peek() == '*'
-                        || operatorStack.peek() == '/')) {
+                        || operatorStack.peek() == '/'
+                        || operatorStack.peek() == '%')) {
                     processAnOperator(operandStack, operatorStack);
                 }
 
                 // Push the * or / operator into the operator stack
+                operatorStack.push(token.charAt(0));
 
+            } else if (token.charAt(0) == '^') {
+                // Process the ^ operator in the top of the operator stack
+                while (!operatorStack.isEmpty() && operatorStack.peek() == '^') {
+                    processAnOperator(operandStack, operatorStack);
+                }
+                // Push the ^ operator into the operator stack
                 operatorStack.push(token.charAt(0));
 
             } else if (token.trim().charAt(0) == '(') {
                 operatorStack.push('('); // Push '(' to stack
 
-                // phase 1.5
-
+            // phase 1.5
             } else if (token.trim().charAt(0) == ')') {
 
                 // Process all the operators in the stack until seeing '('
-
                 while (operatorStack.peek() != '(') {
                     processAnOperator(operandStack, operatorStack);
                 }
 
                 operatorStack.pop(); // Pop the '(' symbol from the stack
 
-                // phase 1.1
-
+            // phase 1.1
             } else { // An operand scanned
 
                 // Push an operand to the stack
@@ -121,7 +116,6 @@ public class EvaluateExpression extends Application {
             }
 
             //output
-
             text.appendText("Operand stack: " + operandStack.toString() + " ");
             text.appendText("Operator stack: " + operatorStack.toString() + "\n");
         }
@@ -139,7 +133,6 @@ public class EvaluateExpression extends Application {
         }
 
         // Return the result
-
         return operandStack.pop();
 
     }
@@ -149,7 +142,8 @@ public class EvaluateExpression extends Application {
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(' || s.charAt(i) == ')' || s.charAt(i) == '+'
                     || s.charAt(i) == '-' || s.charAt(i) == '*'
-                    || s.charAt(i) == '/') {
+                    || s.charAt(i) == '/' || s.charAt(i) == '%'
+                    || s.charAt(i) == '^'){
                 result.append(" ");
                 result.append(s.charAt(i));
                 result.append(" ");
@@ -170,9 +164,7 @@ public class EvaluateExpression extends Application {
      *
      */
 
-    public void processAnOperator(Stack<Integer> operandStack,
-
-                                  Stack<Character> operatorStack) {
+    public void processAnOperator(Stack<Integer> operandStack, Stack<Character> operatorStack) {
 
         char op = operatorStack.pop();
 
@@ -195,6 +187,13 @@ public class EvaluateExpression extends Application {
         else if (op == '/')
 
             operandStack.push(op2 / op1);
+        else if (op == '%')
+
+            operandStack.push(op2 % op1);
+
+        else if (op == '^')
+
+            operandStack.push( (int) Math.pow(op2, op1));
 
     }
 
